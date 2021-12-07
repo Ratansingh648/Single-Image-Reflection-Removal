bbcab4674db99e262fc3146d9d38a10b911ac65f
Ratansingh648 <ratansingh648@gmail.com>
2021-12-06 21:44:06 -0500

    Removing debug prints

--- Ratan/relative_layer_smoothness.py
+++ Ratan/relative_layer_smoothness.py
@@ -57,13 +57,11 @@ def psf2otf(psf, shape):
         return np.zeros_like(psf)
 
     inshape = psf.shape
-    # Pad the PSF to outsize
     psf = zero_pad(psf, shape, position='corner')
 
     for axis, axis_size in enumerate(inshape):
         psf = np.roll(psf, -int(axis_size / 2), axis=axis)
 
-    # Compute the OTF
     otf = np.fft.fft2(psf)
 
     n_ops = np.sum(psf.size * np.log2(psf.shape))
@@ -79,9 +77,7 @@ def im2double(im):
     except:
         N,M = im.shape
         D = 1
-    print(N,M,D)
     for i in range(D):
-      print(i)
       min_pixel = np.min(im[:,:,i])
       max_pixel = np.max(im[:,:,i])
       im[:,:,i] = (im[:,:,i] - min_pixel)/(max_pixel - min_pixel)
@@ -91,27 +87,19 @@ def im2double(im):
 
 
 def imfilter(I, f):
-  #-p[[1,2,3,4,0],:] - f2 (Reorder rows) (2,1)
-  #-p[:,[1,2,3,4,0]] -f1 (Reorder columns) (1,2)
-  # p - f3
-
   try:
     m,n = f.shape
-    print("m - {}, n - {}".format(m,n))
     M, N = I.shape
     if m == 1:
       order = list(range(1,N)) + [0]
       Y = -convolve2d(I, f, mode='same', boundary='wrap')
-      print(Y.shape)
       Y = Y[:, order]
     elif n == 1:
       order = list(range(1,M)) + [0]
       Y = -convolve2d(I, f, mode='same', boundary='wrap')
-      print(Y.shape)
       Y = Y[order, :]
     else:
       Y = convolve2d(I, f, mode='same', boundary='wrap')
-    print("Y Shape - {}".format(Y.shape))    
     return Y
   except Exception as e:
     print(str(e))
@@ -135,7 +123,7 @@ def ifft2(ff):
 
 
 def reflection_removal(I, lamda, lb, hb, L1_0 = None):
-    N, M, D = I.shape;
+    N, M, D = I.shape
     
     # filters
     f1 = np.array([[1, -1]])
@@ -152,15 +140,7 @@ def reflection_removal(I, lamda, lb, hb, L1_0 = None):
     Normin1 = np.multiply(np.repeat(abs(otfL)[:, :, np.newaxis], 3, axis = 2)**2, fft2(I))
     Denormin1 = abs(otfL)**2 
     Denormin2 = abs(otfFx)**2 + abs(otfFy )**2
-    
-    print("-*"*10)
-    print(np.sum(fft2(I)))
-    print(np.sum(np.repeat(abs(otfL)[:, :, np.newaxis], 3, axis = 2)))
-    print(np.sum(Denormin1))
-    print(np.sum(Denormin2))
-    print("-*"*10)
-
-    
+        
     if D>1:
         Denormin1 = np.repeat(Denormin1[:,:, np.newaxis], D, axis=2)
         Denormin2 = np.repeat(Denormin2[:,:, np.newaxis], D, axis=2)
@@ -178,96 +158,26 @@ def reflection_removal(I, lamda, lb, hb, L1_0 = None):
         beta = 2**(i-1)/thr
         Denormin   = lamda*Denormin1 + beta*Denormin2  # This is correct
         
-        print(" Printing the BETA - {}".format(beta))
-        print("Printing Sum of Denormin - {}".format(np.sum(Denormin)))
-        
-
         # update g
         gFx = np.zeros(L1.shape)
         gFy = np.zeros(L1.shape)
 
         for channel in range(D):
-            print("-"*20)
-            print("Processing Channel - {}".format(channel))
-            print("Printing Shape of L1[:,:,channel] - {}".format(L1[:,:,channel].shape))
-            print("Printing the shape of filter - {}".format(f1.shape))
-
-            gFx[:,:, channel] = -1.0*imfilter(L1[:,:,channel],f1);
-            gFy[:,:, channel] = -1.0*imfilter(L1[:,:,channel],f2);
-
-        print("sum of gFx {}".format(np.sum(gFx)))
-        print("sum of gFy {}".format(np.sum(gFy)))
-        
-        #gL = imfilter(L1,f3,'circular');
-        print(gFx.shape)
-        print(gFx[0,:,0])
-        print((np.sum(abs(gFx),axis = 2).shape))
+            gFx[:,:, channel] = -1.0*imfilter(L1[:,:,channel],f1)
+            gFy[:,:, channel] = -1.0*imfilter(L1[:,:,channel],f2)
 
         t = np.repeat((np.sum(abs(gFx),axis = 2)<1.0/beta)[:, :, np.newaxis], D, axis = 2)
-
-        gFx[t] = 0;
-
-        print("Sum of the t - data - {}".format(np.sum(t)))
+        gFx[t] = 0
 
         t = np.repeat((np.sum(abs(gFy),axis = 2)<1.0/beta)[:,:, np.newaxis], D, axis = 2)
+        gFy[t] = 0
 
-        gFy[t] = 0;
-
-        print("Sum of the t - data - {}".format(np.sum(t)))
-        
-        print("sum of gFx {}".format(np.sum(gFx)))
-        print("sum of gFy {}".format(np.sum(gFy)))
-
-    
-        #Correct till here
-
-        
-    #     t = repmat(sum(abs(gL),3)<1/beta,[1 1 D]);
-    #     gL(t) = 0;
-        
-        # compute L1
-    
-        #     ((1,28,3),(27,28,3) ,axis = 0)
-        print(np.sum(gFx[:,-1,:] - gFx[:, 0,:]))
-        rms = -np.diff(gFx,1,1)
-        print(rms.shape)
-        print(np.sum(rms))
-        Normin2 = np.concatenate(((gFx[:,-1,:] - gFx[:, 0,:]).reshape(-1,1,D), -np.diff(gFx,1,1)), axis = 1)
-                
-        print("Sum of normin 2 - {}".format(np.sum(Normin2)))
-        
-        print(Normin2.shape)
-        #Normin2 = Normin2 + [[gFy[-1,:,:] - gFy[0, :,:]], -np.diff(gFy,1,0)]
-        
-        #((28,1,3),(28,27,3), axis = 1)
-        print(np.sum(gFy[-1,:,:] - gFy[0, :,:]))
-        rms = -np.diff(gFy,1,0)
-        print(rms.shape)
-        print(np.sum(rms))
-        
+        Normin2 = np.concatenate(((gFx[:,-1,:] - gFx[:, 0,:]).reshape(-1,1,D), -np.diff(gFx,1,1)), axis = 1)                
         Normin2 = Normin2 + np.concatenate(((gFy[-1,:,:] - gFy[0,:,:]).reshape(1,-1,D), -np.diff(gFy,1,0)), axis = 0)
-        print("Sum of normin 2 - {}".format(np.sum(Normin2)))
-        print(Normin2.shape)
-        
-        #Correct till here
-        
-        print(eps)
-        print(Normin1.shape)
-        print(np.sum(Normin1))
-        print(np.sum(lamda*Normin1))
-        
-        # Correct till here
-        
-        print(np.sum(beta*fft2(Normin2)))
-        print(np.sum(Denormin+eps))
 
         FL1 = np.divide((lamda*Normin1 + beta*fft2(Normin2)),(Denormin+eps))
-        print("Sum of FL1 {}".format(np.sum(FL1)))
-
         L1 = np.real(ifft2(FL1))
         
-        print("Sum of L1 {}".format(np.sum(L1)))
-
         # normalize L1
         for c in range(0,D):
             L1t = L1[:,:,c]
@@ -284,11 +194,10 @@ def reflection_removal(I, lamda, lb, hb, L1_0 = None):
         L1[t] = hb[t]
         
         # L2
-        L2 = I-L1;
+        L2 = I-L1
 
     return L1, L2
 
-#%%
 
 if __name__ == "__main__":
     file_path = "<path to file>"
