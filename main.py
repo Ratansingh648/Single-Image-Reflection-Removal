9f17b107916adaddfdc563e80514c4347728c9b2
Ratansingh648 <ratansingh648@gmail.com>
2021-12-30 17:52:22 -0500

    Removing Debug prints

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ if __name__ == "__main__":
 
     h = 0.033
     e = 1e-10
-    img = cv2.imread('test data\\sign_board.jpg')  # Read image here
-    out = im2double(img.astype('float'))  # Convert to normalized floating poin
+    img = cv2.imread("test data\\sign_board.jpg")  # Read image here
+    out = im2double(img.astype("float"))  # Convert to normalized floating poin
 
     out = reflectSuppress(out, h, e)
 
     L1, _ = reflection_removal(out, 1, np.zeros(out.shape), out, out)
     L2 = out - L1
     cv2.imshow("Original Image", img)
+
+    # There could be slight decrease in brightness. Consider mulipying a suitable factor to L1 while display if needed
+    # E.g. cv2.imshow("Image without Reflection", L1*1.2)
     cv2.imshow("Image without Reflection", L1)
     cv2.imshow("Reflection of Image", L2)
+
+    # To hold the image windows closing when ran from command line / VSCode. Not necessarily needed when running from Spyder
+    cv2.waitKey()
